5205abf061fb5baa7e548a03db67c5b517bd20f2
caok <caok1231@gmail.com>
2014-06-07 15:51:10 +0800

    update config

--- config.js
+++ config.js
@@ -11,12 +11,11 @@ var config = {
     },
     wechat:{
         accessToken:'11235813',
-        URL:'http://wechat.lashare.cn',
-        AppId:'wx125415047174c1cc',
-        AppSecret:'70c86542368f421cbf5f617dc0859c0a'
+        URL:'http://wechat.martz.cn',
+        AppId:'wx8e9801bd8d702d7e',
+        AppSecret:'9219df7022fcd183dae5b4130d55bc09'
     },
-    port:3700
-    //port:9700
+    port:9002
 };
 
 module.exports = config;
